1cedc0ea0ea2e108a334f522b61ae647ba6b4b56
ryanwolhuter <ryanw@linumlabs.com>
2021-05-24 19:20:00 +0200

    textures

--- main.js
+++ main.js
@@ -1,27 +1,56 @@
-import './style.css'
-import * as THREE from 'three'
-import * as dat from 'dat.gui'
-import { resizeRendererToDisplaySize } from 'helpers'
 
-const gui = new dat.GUI()
+import './style.css'
+import {
+  WebGLRenderer,
+  PerspectiveCamera,
+  Scene,
+  BoxGeometry,
+  TextureLoader,
+  MeshBasicMaterial,
+  Mesh
+}from 'three'
 
 function main() {
-  console.log('starting...')
-
   const canvas = document.querySelector('#c')
-  const renderer = new THREE.WebGLRenderer({ canvas })
-  const scene = new THREE.Scene()
+  const renderer = new WebGLRenderer({ canvas })
 
-  const fov = 40
-  const aspect = 2
+  const fov = 75
+  const aspect = 2  // the canvas default
   const near = 0.1
-  const far = 1000
+  const far = 5
+  const camera = new PerspectiveCamera(fov, aspect, near, far)
+  camera.position.z = 2
 
-  const camera = new THREE.PerspectiveCamera(fov, aspect, near, far)
+  const scene = new Scene()
 
-  renderer.render(scene, camera)
+  const boxWidth = 1
+  const boxHeight = 1
+  const boxDepth = 1
+  const geometry = new BoxGeometry(boxWidth, boxHeight, boxDepth)
+
+  const cubes = []  // just an array we can use to rotate the cubes
+  const loader = new TextureLoader()
+
+  const material = new MeshBasicMaterial({
+    map: loader.load('./wall.jpeg'),
+  })
+  const cube = new Mesh(geometry, material)
+  scene.add(cube)
+  cubes.push(cube)  // add to our list of cubes to rotate
+
+  function resizeRendererToDisplaySize(renderer) {
+    const canvas = renderer.domElement
+    const width = canvas.clientWidth
+    const height = canvas.clientHeight
+    const needResize = canvas.width !== width || canvas.height !== height
+    if (needResize) {
+      renderer.setSize(width, height, false)
+    }
+    return needResize
+  }
 
   function render(time) {
+    time *= 0.001
 
     if (resizeRendererToDisplaySize(renderer)) {
       const canvas = renderer.domElement
@@ -29,7 +58,15 @@ function main() {
       camera.updateProjectionMatrix()
     }
 
+    cubes.forEach((cube, ndx) => {
+      const speed = .2 + ndx * .1
+      const rot = time * speed
+      cube.rotation.x = rot
+      cube.rotation.y = rot
+    })
+
     renderer.render(scene, camera)
+
     requestAnimationFrame(render)
   }
 
